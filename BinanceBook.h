435f4f6acb5d62a4d40f84b254aa622cf7ffa3fe
popina1994 <zd130033d@student.etf.bg.ac.rs>
2023-07-10 22:45:38 +0100

    1. Initial logic with insertion sort. 2. Adding further logic.

--- BinanceBook.h
+++ BinanceBook.h
@@ -1,46 +1,133 @@
 #pragma once
 
-#include "BookTicker.h"
 
 #include <vector>
 #include <string>
+#include "BookTicker.h"
+#include "BookDepth.h"
+#include <sstream>
+#include <format>
 
 namespace Pulsar
 {
     // template?
     class BinanceBook
     {
+        
+        BookDepth m_bookDepth;
+        // I did not understand what is the minimal size of books to be held. 
+        // In order to reduce the latency I would keep just a specific number of bids/offers (up to 20).
     public:
+
+        BinanceBook(){}
+
         // Clear the book
         void clear(void)
         {
-
+            m_bookDepth.clear();
         }
 
         // Test whether book is empty.
         bool is_empty(void) const
         {
-            return false;
+            return (m_bookDepth.m_nBids == 0) && (m_bookDepth.m_nAsks == 0);
         }
 
         // Replace entire contents of book with given bids / asks (assumed to be in canonical order).
-        // Bonus: accept any suitable container of PriceQuantity
-        void replace(const std::vector<BookTicker>& vBookTicker)
+        // Bonus: accept any suitable container of 
+        // TODO: https://stackoverflow.com/questions/7728478/c-template-class-function-with-arbitrary-container-type-how-to-define-it
+        void replace(const std::vector<PriceQuantity>& vAsks, const std::vector<PriceQuantity>& vBids)
         {
+            m_bookDepth.m_nAsks = vAsks.size();
+            uint32_t idx = 0; 
+            for (const auto& ask : vAsks)
+            {
+                m_bookDepth.m_vAsks[idx] = ask;
+                idx++;
+            }
 
+            m_bookDepth.m_nBids = vBids.size();
+            idx = 0;
+            for (const auto& bid: vBids)
+            {
+                m_bookDepth.m_vBids[idx] = bid;
+                idx++;
+            }
+        }
+
+        void replace(const BookDepth& bookDepth)
+        {
+            m_bookDepth = bookDepth;
         }
 
         // Apply a new best bid / ask.
         void update_bbo(const PriceQuantity& newBid, const PriceQuantity& newAsk)
         {
+            uint32_t idxBid;
+            uint32_t idxAsk;
+            bool cutBids = false;
+            bool equalBid = false;
+            bool cutAsks = false;
+            bool equalAsk = false;
 
+            for (idxBid = m_bookDepth.m_nBids; idxBid > 0; idxBid--)
+            {
+                if (m_bookDepth.m_vBids[idxBid].price > newBid.price)
+                {
+                    cutBids = true;
+                    break;
+                }
+                if (m_bookDepth.m_vBids[idxBid].price == newBid.price)
+                {
+                    equalBid = true;
+                    m_bookDepth.m_vBids[idxBid].quantity = newBid.quantity;
+                    break;
+                }
+            }
+            if (cutBids)
+            {
+                if (idxBid == 0)
+                {
+                    for (int32_t idx = m_bookDepth.m_nBids - 1; idx >= idxBid; idx--)
+                    {
+                        m_bookDepth.m_vBids[idx + 1] = m_bookDepth.m_vBids[idx];
+                    }
+                }
+                m_bookDepth.m_vBids[0] = newBid;
+            }
+
+            for (idxAsk = m_bookDepth.m_nAsks; idxAsk > 0; idxAsk--)
+            {
+                if (m_bookDepth.m_vAsks[idxAsk].price < newAsk.price)
+                {
+                    cutAsks = true;
+                    break;
+                }
+                if (m_bookDepth.m_vAsks[idxAsk].price == newAsk.price)
+                {
+                    equalBid = true;
+                    m_bookDepth.m_vAsks[idxAsk].quantity = newAsk.quantity;
+                    break;
+                }
+            }
+            if (cutAsks)
+            {
+                if (idxAsk == 0)
+                {
+                    for (int32_t idx = m_bookDepth.m_nAsks - 1; idx >= idxAsk; idx--)
+                    {
+                        m_bookDepth.m_vAsks[idx + 1] = m_bookDepth.m_vAsks[idx];
+                    }
+                }
+                m_bookDepth.m_vAsks[0] = newAsk;
+            }
         }
 
         // Retrieve the book (in canonical order).
         // This should output something similar to the input for `replace()`.
-        std::vector<BookTicker> extract(void)
+        const BookDepth& extract(void)
         {
-            return {};
+            return m_bookDepth;
         }
 
 
@@ -48,7 +135,19 @@ namespace Pulsar
         // This should be efficient but isn't performance critical.
         const std::string to_string(void) const
         {
-            return "";
+            std::stringstream ss;
+            uint32_t maxEntries = std::max(m_bookDepth.m_nAsks, m_bookDepth.m_nBids);
+            for (int idx = 0; idx < maxEntries; idx++)
+            {
+                std::format("{:<6}", 42);
+                ss << "[" << std::format("{:<2}", idx + 1) << "] ";
+                ss << "[" << std::format("{:<7}", m_bookDepth.m_vBids[idx].quantity) << "]";
+                ss << " " << std::format("{:<7}", m_bookDepth.m_vBids[idx].price) << " | ";
+                ss << " " << std::format("{:<7}", m_bookDepth.m_vAsks[idx].price);
+                ss << "[" << std::format("{:<7}", m_bookDepth.m_vAsks[idx].quantity) << "]";
+                ss << std::endl;
+                return ss.str();
+            }
         }
     };
 }
--- BookDepth.h
+++ BookDepth.h
@@ -1,13 +1,27 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
 #include "PriceQuantity.h"
 
 namespace Pulsar
 {
 	struct BookDepth
 	{
-		std::vector<PriceQuantity> bids;
-		std::vector<PriceQuantity> asks; // or could be e.g. boost::static_vector/small_vector
+		static constexpr uint32_t NUM_BIDS_TO_KEEP = 21;
+
+		std::vector<PriceQuantity> m_vBids;
+		std::vector<PriceQuantity> m_vAsks; // or could be e.g. boost::static_vector/small_vector
+		uint32_t m_nBids = 0;
+		uint32_t m_nAsks = 0;
+		
+		BookDepth(): m_vBids(NUM_BIDS_TO_KEEP), m_vAsks(NUM_BIDS_TO_KEEP) {}
+
+		void clear(void) { 
+			m_nBids = 0;
+			m_nAsks = 0;
+			std::fill(m_vBids.begin(), m_vBids.end(), 0.0);
+			std::fill(m_vAsks.begin(), m_vAsks.end(), 0.0);
+		}
 	};
 }
